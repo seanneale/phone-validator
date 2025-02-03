fb32a2698b9d0f55006108b81609fa53c2bacf57
Sean Neale <sean_neale@hotmail.co.uk>
2025-02-03 11:12:15 +0800

    chore/add_rspec: remove tests

--- spec/test_spec.rb
+++
@@ -1,7 +0,0 @@
-RSpec.describe 'Test' do           #
-  context 'before publication' do  # (almost) plain English
-    it 'cannot have comments' do   #
-      expect { false }.to be_truthy  # test code
-    end
-  end
-end
