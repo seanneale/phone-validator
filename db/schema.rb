1f1acd0c5c18752ae51d43264b337aafda55c782
Sean Neale <sean_neale@hotmail.co.uk>
2025-02-03 12:31:01 +0800

    chore/countries: add iso list of countries

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,5 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 0) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_03_041558) do
 end
